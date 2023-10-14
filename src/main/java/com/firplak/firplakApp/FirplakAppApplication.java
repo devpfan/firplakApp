575b13dbce3ba5805a18306656b5dedb3c0347ff
devpfan <128647947+devpfan@users.noreply.github.com>
2023-10-14 15:19:34 -0500

    Initial commit

---
+++ src/main/java/com/firplak/firplakApp/FirplakAppApplication.java
@@ -0,0 +1,13 @@
+package com.firplak.firplakApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirplakAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirplakAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/firplak/firplakApp/FirplakAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.firplak.firplakApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirplakAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
